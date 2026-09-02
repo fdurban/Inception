c9802b1202b907fb3b81755840261befc33d3f82
fdurban- <fdurban-@fdurban.fdurban-.42.fr>
2026-09-02 17:52:31 +0200

    first container done and done research of php fpm

--- srcs/requirements/mariadb/entrypoint.sh
+++ srcs/requirements/mariadb/entrypoint.sh
@@ -4,4 +4,30 @@ sed -i "s|*.bind-address\s*=.*|bind-address=0.0.0.0|g" /etc/my.cnf.d/mariadb-ser
 
 sed -i "s|skip-networking|# skip-networking|g" /etc/my.cnf.d/mariadb-server.cnf
 
+if [ ! -d /var/lib/mysql/mysql ] ; then
+	echo "Data directory empy... Initializing MariaDB";
+	mariadb-install-db --user=mysql --datadir=/var/lib/mysql > /dev/null
 
+	mysqld --user=mysql --datadir=/var/lib/mysql  --skip-networking  &
+	pid="$!"
+	
+	while ! mysqladmin ping --silent; do
+		sleep 1
+	done
+
+	mysql -u root <<-EOSQL
+		CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;
+		CREATE USER IF NOT EXISTS '${SQL_USER}'@'%' IDENTIFIED BY '${SQL_PASSWORD}';
+		GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO '${SQL_USER}'@'%';
+		ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';
+		FLUSH PRIVILEGES;
+EOSQL
+
+	mysqladmin -u root -p"${SQL_ROOT_PASSWORD}" shutdown
+	wait"$pid"
+
+	echo "Mariadb initialization completed"
+
+fi
+
+exec "$@"
